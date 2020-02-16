22f02ffb2152ce9421255d49318a4ecbb5549b74
danm1kael <61100747+danm1kael@users.noreply.github.com>
2020-02-15 23:46:39 -0300

    Add files via upload

---
+++ monstros-vs-human/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1581820276,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/tiledbackground3.png",
+		"images/jogador-sheet0.png",
+		"images/monstro-sheet0.png",
+		"images/bala-sheet0.png",
+		"images/explosão-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/shot.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
