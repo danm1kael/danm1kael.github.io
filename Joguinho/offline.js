26555e30588fcc5a5104d65b7caa29ad80116e95
danm1kael <61100747+danm1kael@users.noreply.github.com>
2020-02-16 22:26:53 -0300

    Add files via upload

---
+++ Joguinho/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1581902605,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tiledbackground.png",
+		"images/pista-sheet0.png",
+		"images/cano-sheet0.png",
+		"images/passaro-sheet0.png",
+		"images/passaro-sheet1.png",
+		"images/fundo-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
